00adfc75b983eafc3c1855d0412b0a8de6be4ff4
Muhammad Husnain Rashid <144523048+Husnain0120@users.noreply.github.com>
2024-08-10 03:50:29 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/': 'https://e-com2-server-side-api.vercel.app',
-      '/uploads/': 'https://e-com2-server-side-api.vercel.app',
+      '/api/': 'https://e-com2-backend.vercel.app/',
+      '/uploads/': 'https://e-com2-backend.vercel.app/',
     },
   },
 });
