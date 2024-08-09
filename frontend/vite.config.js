05dfac78407cc01c42c547cd4f76b8101ccfb152
Muhammad Husnain Rashid <muhammadhusnainrashid.bs@gmail.com>
2024-08-09 19:13:02 +0500

    add backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://e-com2-server-side-api.vercel.app",
+      "/uploads/": "https://e-com2-server-side-api.vercel.app",
     },
   },
 });
